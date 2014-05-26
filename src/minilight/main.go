33103e2c3efa0c540caf51185dcb98ef3927b099
James Tauber <jtauber@jtauber.com>
2014-05-26 16:54:33 -0400

    go fmt

--- src/minilight/main.go
+++ src/minilight/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "image/png"
-    "minilight/radiance"
-    "os"
+	"image/png"
+	"minilight/radiance"
+	"os"
 )
 
 func main() {
-    ri := radiance.NewRadianceImage(500, 500)
-    for x := 0; x < 500; x++ {
+	ri := radiance.NewRadianceImage(500, 500)
+	for x := 0; x < 500; x++ {
 		for y := 0; y < 500; y++ {
-            ri.Add(x, y, radiance.Radiance{1, 0, 0})
+			ri.Add(x, y, radiance.Radiance{1, 0, 0})
 		}
 	}
-    im := ri.ToRGBA64(1);
+	im := ri.ToRGBA64(1)
 
-    w, err := os.Create("test.png")
+	w, err := os.Create("test.png")
 	if err != nil {
 		panic(err)
 	}
--- src/minilight/radiance/radiance.go
+++ src/minilight/radiance/radiance.go
@@ -41,38 +41,38 @@ func (p *RadianceImage) Add(x int, y int, radiance Radiance) {
 
 func (p *RadianceImage) Get(x int, y int) Radiance {
 	i := p.pixOffset(x, y)
-	return Radiance{p.pix[i + 0], p.pix[i + 1], p.pix[i + 2]}
+	return Radiance{p.pix[i+0], p.pix[i+1], p.pix[i+2]}
 }
 
 func (p *RadianceImage) calculateScaleFactor(iterations int) float64 {
-    // calculate the linear tone-mapping scalefactor for this image assuming
-    // the given number of iterations.
+	// calculate the linear tone-mapping scalefactor for this image assuming
+	// the given number of iterations.
 
-    // calculate the log-mean luminance of the image
+	// calculate the log-mean luminance of the image
 
 	var sum_of_logs float64
 
 	for x := 0; x < p.Width; x++ {
 		for y := 0; y < p.Height; y++ {
-            lum := p.pix[p.pixOffset(x, y) + 0] * R_LUMINANCE
-            lum += p.pix[p.pixOffset(x, y) + 1] * G_LUMINANCE
-            lum += p.pix[p.pixOffset(x, y) + 2] * B_LUMINANCE
-            lum /= float64(iterations)
+			lum := p.pix[p.pixOffset(x, y)+0] * R_LUMINANCE
+			lum += p.pix[p.pixOffset(x, y)+1] * G_LUMINANCE
+			lum += p.pix[p.pixOffset(x, y)+2] * B_LUMINANCE
+			lum /= float64(iterations)
 
-            sum_of_logs += math.Log10(math.Max(lum, 0.0001))
+			sum_of_logs += math.Log10(math.Max(lum, 0.0001))
 		}
 	}
 
-    log_mean_luminance := math.Pow(10.0, (sum_of_logs / float64(p.Height * p.Width)))
+	log_mean_luminance := math.Pow(10.0, (sum_of_logs / float64(p.Height*p.Width)))
 
-    // calculate the scalefactor for linear tone-mapping
-    // formula from Ward "A Contrast-Based Scalefactor for Luminance Display"
+	// calculate the scalefactor for linear tone-mapping
+	// formula from Ward "A Contrast-Based Scalefactor for Luminance Display"
 
-	scalefactor_numerator := 1.219 + math.Pow(DISPLAY_LUMINANCE_MAX * 0.25, 0.4)
+	scalefactor_numerator := 1.219 + math.Pow(DISPLAY_LUMINANCE_MAX*0.25, 0.4)
 
-    scalefactor := (math.Pow((scalefactor_numerator / (1.219 + math.Pow(log_mean_luminance, 0.4))), 2.5)) / DISPLAY_LUMINANCE_MAX
+	scalefactor := (math.Pow((scalefactor_numerator / (1.219 + math.Pow(log_mean_luminance, 0.4))), 2.5)) / DISPLAY_LUMINANCE_MAX
 
-    return scalefactor
+	return scalefactor
 }
 
 func (p *RadianceImage) ToRGBA64(iterations int) *image.RGBA64 {
@@ -83,9 +83,9 @@ func (p *RadianceImage) ToRGBA64(iterations int) *image.RGBA64 {
 	for x := 0; x < p.Width; x++ {
 		for y := 0; y < p.Height; y++ {
 			radiance := p.Get(x, y)
-			r := uint16(65535 * math.Pow(math.Max(radiance.R * scalefactor / float64(iterations), 0), GAMMA_ENCODE))
-			g := uint16(65535 * math.Pow(math.Max(radiance.G * scalefactor / float64(iterations), 0), GAMMA_ENCODE))
-			b := uint16(65535 * math.Pow(math.Max(radiance.B * scalefactor / float64(iterations), 0), GAMMA_ENCODE))
+			r := uint16(65535 * math.Pow(math.Max(radiance.R*scalefactor/float64(iterations), 0), GAMMA_ENCODE))
+			g := uint16(65535 * math.Pow(math.Max(radiance.G*scalefactor/float64(iterations), 0), GAMMA_ENCODE))
+			b := uint16(65535 * math.Pow(math.Max(radiance.B*scalefactor/float64(iterations), 0), GAMMA_ENCODE))
 			a := uint16(65535)
 			m.Set(x, y, color.RGBA64{r, g, b, a})
 		}
