943fd7e8c0296b63b39e7e98c728c3aec77534a2
James Tauber <jtauber@jtauber.com>
2014-05-26 15:49:59 -0400

    reordered functions and made some things private

--- radiance.go
+++ radiance.go
@@ -6,21 +6,21 @@ type Radiance struct {
 
 type RadianceImage struct {
 	Width, Height int
-	Pix           []float64
-}
-
-func (p *RadianceImage) PixOffset(x, y int) int {
-	return y*p.Width*3 + x*3
-}
-
-func (p *RadianceImage) Add(x int, y int, radiance Radiance) {
-	i := p.PixOffset(x, y)
-	p.Pix[i+0] += radiance.R
-	p.Pix[i+1] += radiance.G
-	p.Pix[i+2] += radiance.B
+	pix           []float64
 }
 
 func NewRadianceImage(width, height int) *RadianceImage {
 	buf := make([]float64, 3*width*height)
 	return &RadianceImage{width, height, buf}
+}
+
+func (p *RadianceImage) pixOffset(x, y int) int {
+	return y*p.Width*3 + x*3
+}
+
+func (p *RadianceImage) Add(x int, y int, radiance Radiance) {
+	i := p.pixOffset(x, y)
+	p.pix[i+0] += radiance.R
+	p.pix[i+1] += radiance.G
+	p.pix[i+2] += radiance.B
 }
