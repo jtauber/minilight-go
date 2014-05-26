6f333e0013e98716833ba85ea07b3411774a5d52
James Tauber <jtauber@jtauber.com>
2014-05-26 16:50:58 -0400

    initial main for testing

---
+++ src/minilight/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "image/png"
+    "minilight/radiance"
+    "os"
+)
+
+func main() {
+    ri := radiance.NewRadianceImage(500, 500)
+    for x := 0; x < 500; x++ {
+		for y := 0; y < 500; y++ {
+            ri.Add(x, y, radiance.Radiance{1, 0, 0})
+		}
+	}
+    im := ri.ToRGBA64(1);
+
+    w, err := os.Create("test.png")
+	if err != nil {
+		panic(err)
+	}
+	png.Encode(w, im)
+}
